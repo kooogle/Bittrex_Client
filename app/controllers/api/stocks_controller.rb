ea63014d64730d0e3f038ca67e52b6a23f847626
Koogle.Zhang <koogle.zhang@gmail.com>
2017-11-02 12:17:12 +0800

    添加自定义返回数量

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -2,8 +2,9 @@ class Api::StocksController < ApplicationController
 
   def quote
     block = params[:block] || Chain.named.first.id
+    amount = params[:amount] || 24
     chain = Chain.find(block)
-    tickers = chain.tickers.last(24)
+    tickers = chain.tickers.last(amount.to_i)
     render json:{title: chain.label, market:chain.full_name,
       time: tickers.map{|x| x.created_at.strftime('%H:%M')},
       price:tickers.map{|x| x.last_price},
