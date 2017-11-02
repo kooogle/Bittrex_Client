dd9ecade3ba02c6f28764797faaf4e5e7e8931d9
Koogle.Zhang <koogle.zhang@gmail.com>
2017-11-02 12:12:44 +0800

    添加多维度参数

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -4,6 +4,11 @@ class Api::StocksController < ApplicationController
     block = params[:block] || Chain.named.first.id
     chain = Chain.find(block)
     tickers = chain.tickers.last(24)
-    render json:{title: chain.label, market:chain.full_name,time: tickers.map{|x| x.created_at.strftime('%H:%M')},price:tickers.map{|x| x.last_price}}
+    render json:{title: chain.label, market:chain.full_name,
+      time: tickers.map{|x| x.created_at.strftime('%H:%M')},
+      price:tickers.map{|x| x.last_price},
+      ma5_price:tickers.map{|x| x.ma5_price},
+      ma10_price:tickers.map{|x| x.ma10_price}
+    }
   end
 end
