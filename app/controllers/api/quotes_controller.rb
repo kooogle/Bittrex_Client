17e784e6ba1377555e96ad6a4e8feed9c623059f
Koogle zhang <koogle.zhang@gmail.com>
2017-11-27 02:11:07 +0800

    添加macd和ma因子买卖

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -5,7 +5,8 @@ class Api::QuotesController < ApplicationController
       item.generate_ticker rescue nil
       extremum_report(item)
       if item.point && item.point.state
-        middle_analysis(item)
+        middle_ma_business(item)
+        middle_macd_business(item)
       end
     end
     render json:{code:200}
@@ -72,43 +73,75 @@ private
   end
 
   #根据 MACD
-  def middle_analysis(block)
+  def middle_macd_business(block)
     market = block.market
     recent = block.tickers.last(7)
     macd_dea_last = recent.map {|x| x.macd_dea }
     macd_diff_last = recent.map {|x| x.macd_diff }
     diff_dea_last = recent.map {|x| x.macd_diff - x.macd_dea }
     if diff_dea_last[-1] < 0 && diff_dea_last[-2] > 0 && diff_dea_last[0..-2].min > 0
-      sell_quote_market(block,market)
+      sell_macd_market(block,market)
     elsif diff_dea_last[-1] > 0 && macd_diff_last[-2] < 0 && diff_dea_last[0..-2].max < 0
-      buy_quote_market(block,market)
+      buy_macd_market(block,market)
     end
   end
 
-  def sell_quote_market(block,market)
+  def sell_macd_market(block,market)
     last_price = market.first['Bid']
-    buy = block.low_buy_business.order(price: :asc).first
+    buy = block.low_buy_business.first
     balance = block.balance
-    if buy
-      if balance > 0 && last_price > buy.price * 1.0618
-        sell_chain(block,buy.amount,last_price)
-      end
-    else
-      if balance > 0
-        sell_chain(block,balance,last_price)
-      end
+    if buy && balance > 0 && last_price > buy.price * 1.03
+      amount = balance > buy.amount ? buy.amount : balance
+      sell_chain(block,amount,last_price)
     end
   end
 
-  def buy_quote_market(block,market)
+  def buy_macd_market(block,market)
     last_price = market.first['Ask']
     point = block.point
-    avl_money = block.money #可用的有效现金
-    buy_money = point.total_value
-    if avl_money > 1
+    avl_money = block.money
+    buy_money = point.low_price
+    total_money = point.total_value
+    had_total = block.low_buy_business.map {|x| x.total}.sum
+    if avl_money > 1 && had_total < total_money
       money = avl_money > buy_money ? buy_money : avl_money
       amount = (money/last_price).to_d.round(5,:truncate).to_f
       buy_chain(block,amount,last_price) if amount > 0
+    end
+  end
+
+  def middle_ma_business(block)
+     market = block.market
+     recent = block.tickers.last(7)
+     ma_diff = recent.map {|x| x.ma5_price - x.ma10_price }
+     if ma_diff[-1] > 0 && diff_dea_last[-2]  < 0
+        buy_ma_market(block,market)
+     elsif  ma_diff[-1] < 0 && diff_dea_last[-2]  > 0
+        sell_ma_market(block,market)
+      end
+  end
+
+  def buy_ma_market(block,market)
+    last_price = market.first['Ask']
+    point = block.point
+    avl_money = block.money
+    buy_money = point.high_price
+    total_money = point.high_value
+    had_total = block.high_buy_business.map {|x| x.high_value}.sum
+    if avl_money > 1 && had_total < total_money
+      money = avl_money > buy_money ? buy_money : avl_money
+      amount = (money/last_price).to_d.round(4,:truncate).to_f
+      high_buy_chain(block,amount,last_price) if amount > 0
+    end
+  end
+
+  def sell_ma_market(block,market)
+    last_price = market.first['Bid']
+    buy = block.high_buy_business.first
+    balance = block.balance
+    if buy && balance > 0 && last_price > buy.price * 1.02
+      amount = balance > buy.amount ? buy.amount : balance
+      high_sell_chain(block,amount,last_price)
     end
   end
 
@@ -146,35 +179,6 @@ private
     end
   end
 
-  def high_analysis(block)
-    market = block.market
-    point = block.point
-    stock = block.tickers.last
-    last_price = stock.last_price #最近半小时报价
-    buy_price = market.first['Ask'] #卖出低单价
-    sell_price = market.first['Bid'] #买入最高单价
-    money = block.money #可用的有效现金
-    balance = block.balance #持有的区块数
-    low_buy = block.high_buy_business.order(price: :asc).first #当前存在的有效买单
-    high_total_val = block.high_buy_business.map {|x| x.total}.sum
-    if low_buy && sell_price > low_buy.price * 1.02 && balance > 0
-      if balance > low_buy.amount
-        high_sell_chain(block,low_buy.amount,sell_price)
-      else
-        high_sell_chain(block,balance,sell_price)
-      end
-    end
-    if buy_price < last_price && high_total_val < point.high_value && money > point.high_price
-      if stock.ma5_price > stock.ma10_price && buy_price  < last_price * 1.005
-        amount = (point.high_price/buy_price).to_d.round(4,:truncate).to_f
-        high_buy_chain(block,amount,buy_price)
-      elsif stock.ma5_price < stock.ma10_price && buy_price * 1.01 < last_price
-        amount = (point.high_price/buy_price).to_d.round(4,:truncate).to_f
-        high_buy_chain(block,amount,buy_price)
-      end
-    end
-  end
-
   def high_buy_chain(block,amount,price)
     order = Order.new
     order.deal = 1
@@ -197,8 +201,8 @@ private
 
   def work_time
     current = Time.now.strftime('%H').to_i
-    return true if current < 22 || current > 10
-    false
+    return true if  current > 9 && current < 22
+    nil
   end
 
 end
